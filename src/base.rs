8009b07fd382fceb44666a4609f7e3dabb59e9db
Christopher Berner <christopherberner@gmail.com>
2019-01-23 22:49:22 -0800

    Implement Tuple[K', X]

--- src/base.rs
+++ src/base.rs
@@ -1,4 +1,9 @@
 use std::cmp::min;
+use systematic_constants::num_intermediate_symbols;
+use systematic_constants::systematic_index;
+use rng::rand;
+use systematic_constants::num_lt_symbols;
+use systematic_constants::calculate_p1;
 
 // As defined in section 3.2
 pub struct PayloadId {
@@ -39,4 +44,36 @@ pub fn deg(v: u32, lt_symbols: u32) -> u32 {
         }
     }
     panic!();
+}
+
+// Tuple[K', X] as defined in section 5.3.5.4
+#[allow(non_snake_case)]
+pub fn intermediate_tuple(source_block_symbols: u32, internal_symbol_id: u32) -> (u32, u32, u32, u32, u32, u32) {
+    let L = num_intermediate_symbols(source_block_symbols);
+    let J = systematic_index(source_block_symbols);
+    let W = num_lt_symbols(source_block_symbols);
+    let P1 = calculate_p1(source_block_symbols);
+
+    let mut A = 53591 + J*997;
+
+    if A % 2 == 0 {
+        A = A + 1
+    }
+
+    let B = 10267*(J + 1);
+    let y: u32 = ((B as u64 + internal_symbol_id as u64 * A as u64) % 4294967296) as u32;
+    let v = rand(y, 0, 1048576);
+    let d = deg(v, W);
+    let a = 1 + rand(y, 1, W-1);
+    let b = rand(y, 2, W);
+
+    let mut d1 = 2;
+    if d < 4 {
+        let d1 = 2 + rand(internal_symbol_id, 3, 2);
+    }
+
+    let a1 = 1 + rand(internal_symbol_id, 4, P1-1);
+    let b1 = rand(internal_symbol_id, 5, P1);
+
+    (d, a, b, d1, a1, b1)
 }
