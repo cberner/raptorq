587d1569280cdabee52ee740ef5339384700b009
Christopher Berner <christopherberner@gmail.com>
2019-01-28 17:24:31 -0800

    Update benchmarks

--- benches/codec_benchmark.rs
+++ benches/codec_benchmark.rs
@@ -10,7 +10,7 @@ use raptorq::SourceBlockDecoder;
 
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let elements = 1024*1024;
+    let elements = 10*1024;
     let symbol_size = 512;
     let mut data: Vec<u8> = vec![0; elements];
     for i in 0..elements {
@@ -18,16 +18,28 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
 
     let encode_data = data.clone();
-    c.bench_function("encode 1MB", move |b| b.iter(|| {
+    c.bench_function("encode 10KB", move |b| b.iter(|| {
         let encoder = SourceBlockEncoder::new(1, symbol_size, encode_data.clone());
         return encoder.all_source_packets();
     }));
 
-    c.bench_function("roundtrip 1MB", move |b| b.iter(|| {
-        let encoder = SourceBlockEncoder::new(1, symbol_size, data.clone());
+    let roundtrip_data = data.clone();
+    c.bench_function("roundtrip 10KB", move |b| b.iter(|| {
+        let encoder = SourceBlockEncoder::new(1, symbol_size, roundtrip_data.clone());
         let mut decoder = SourceBlockDecoder::new(1, symbol_size, elements as u64);
         let mut result = None;
         for packet in encoder.all_source_packets() {
+            result = decoder.parse(packet);
+        }
+        return result
+    }));
+
+    let repair_data = data.clone();
+    c.bench_function("roundtrip repair 10KB", move |b| b.iter(|| {
+        let encoder = SourceBlockEncoder::new(1, symbol_size, repair_data.clone());
+        let mut decoder = SourceBlockDecoder::new(1, symbol_size, elements as u64);
+        let mut result = None;
+        for packet in encoder.repair_packets(0, (elements / symbol_size as usize) as u32) {
             result = decoder.parse(packet);
         }
         return result
