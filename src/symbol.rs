4c084c03efe941c1f93bc9249b8fb2008f493a2f
Christopher Berner <christopherberner@gmail.com>
2019-02-03 14:53:59 -0800

    Make Symbol division test pass deterministically

--- src/symbol.rs
+++ src/symbol.rs
@@ -149,7 +149,7 @@ mod tests {
         let elements = 4;
         let mut data: Vec<u8> = vec![0; elements];
         for i in 0..elements {
-            data[i] = rand::thread_rng().gen();
+            data[i] = rand::thread_rng().gen_range(1, 255);
         }
         let symbol = Symbol::new(data);
         let symbol2 = symbol.clone();
