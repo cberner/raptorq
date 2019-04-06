bdf5627e4cdd2d1f9cb63aaf36028b52d4c55950
Anders Martinsson <anders.martinsson@intinor.se>
2019-04-06 15:39:07 +0200

    Add SSSE3 SIMD implementation
    Add SSSE3 support to be used if AVX2 support is missing and SSSE3 is available.
    
    Signed-off-by: Anders Martinsson <anders.martinsson@intinor.se>

--- src/octets.rs
+++ src/octets.rs
@@ -60,12 +60,57 @@ unsafe fn mulassign_scalar_avx2(octets: &mut [u8], scalar: &Octet) {
     }
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "ssse3")]
+unsafe fn mulassign_scalar_ssse3(octets: &mut [u8], scalar: &Octet) {
+    #[cfg(target_arch = "x86")]
+    use std::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use std::arch::x86_64::*;
+
+    let low_mask = _mm_set1_epi8(0x0F);
+    let hi_mask = _mm_set1_epi8(0xF0 as u8 as i8);
+    let self_ssse_ptr = octets.as_mut_ptr();
+    #[allow(clippy::cast_ptr_alignment)]
+    let low_table =
+        _mm_loadu_si128(OCTET_MUL_LOW_BITS[scalar.byte() as usize].as_ptr() as *const __m128i);
+    #[allow(clippy::cast_ptr_alignment)]
+    let hi_table =
+        _mm_loadu_si128(OCTET_MUL_HI_BITS[scalar.byte() as usize].as_ptr() as *const __m128i);
+
+    for i in 0..(octets.len() / 16) {
+        #[allow(clippy::cast_ptr_alignment)]
+        let self_vec = _mm_loadu_si128((self_ssse_ptr as *const __m128i).add(i));
+        let low = _mm_and_si128(self_vec, low_mask);
+        let low_result = _mm_shuffle_epi8(low_table, low);
+        let hi = _mm_and_si128(self_vec, hi_mask);
+        let hi = _mm_srli_epi64(hi, 4);
+        let hi_result = _mm_shuffle_epi8(hi_table, hi);
+        let result = _mm_xor_si128(hi_result, low_result);
+        #[allow(clippy::cast_ptr_alignment)]
+        _mm_storeu_si128((self_ssse_ptr as *mut __m128i).add(i), result);
+    }
+
+    let remainder = octets.len() % 16;
+    let scalar_index = scalar.byte() as usize;
+    for i in (octets.len() - remainder)..octets.len() {
+        *octets.get_unchecked_mut(i) = *OCTET_MUL
+            .get_unchecked(scalar_index)
+            .get_unchecked(*octets.get_unchecked(i) as usize);
+    }
+}
+
 pub fn mulassign_scalar(octets: &mut [u8], scalar: &Octet) {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
         if is_x86_feature_detected!("avx2") {
             unsafe {
                 return mulassign_scalar_avx2(octets, scalar);
+            }
+        }
+        if is_x86_feature_detected!("ssse3") {
+            unsafe {
+                return mulassign_scalar_ssse3(octets, scalar);
             }
         }
     }
@@ -133,6 +178,53 @@ unsafe fn fused_addassign_mul_scalar_avx2(octets: &mut [u8], other: &[u8], scala
     }
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "ssse3")]
+unsafe fn fused_addassign_mul_scalar_ssse3(octets: &mut [u8], other: &[u8], scalar: &Octet) {
+    #[cfg(target_arch = "x86")]
+    use std::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use std::arch::x86_64::*;
+
+    let low_mask = _mm_set1_epi8(0x0F);
+    let hi_mask = _mm_set1_epi8(0xF0 as u8 as i8);
+    let self_ssse_ptr = octets.as_mut_ptr();
+    let other_ssse_ptr = other.as_ptr();
+    #[allow(clippy::cast_ptr_alignment)]
+    let low_table =
+        _mm_loadu_si128(OCTET_MUL_LOW_BITS[scalar.byte() as usize].as_ptr() as *const __m128i);
+    #[allow(clippy::cast_ptr_alignment)]
+    let hi_table =
+        _mm_loadu_si128(OCTET_MUL_HI_BITS[scalar.byte() as usize].as_ptr() as *const __m128i);
+
+    for i in 0..(octets.len() / 16) {
+        // Multiply by scalar
+        #[allow(clippy::cast_ptr_alignment)]
+        let other_vec = _mm_loadu_si128((other_ssse_ptr as *const __m128i).add(i));
+        let low = _mm_and_si128(other_vec, low_mask);
+        let low_result = _mm_shuffle_epi8(low_table, low);
+        let hi = _mm_and_si128(other_vec, hi_mask);
+        let hi = _mm_srli_epi64(hi, 4);
+        let hi_result = _mm_shuffle_epi8(hi_table, hi);
+        let other_vec = _mm_xor_si128(hi_result, low_result);
+
+        // Add to self
+        #[allow(clippy::cast_ptr_alignment)]
+        let self_vec = _mm_loadu_si128((self_ssse_ptr as *const __m128i).add(i));
+        let result = _mm_xor_si128(self_vec, other_vec);
+        #[allow(clippy::cast_ptr_alignment)]
+        _mm_storeu_si128((self_ssse_ptr as *mut __m128i).add(i), result);
+    }
+
+    let remainder = octets.len() % 16;
+    let scalar_index = scalar.byte() as usize;
+    for i in (octets.len() - remainder)..octets.len() {
+        *octets.get_unchecked_mut(i) ^= *OCTET_MUL
+            .get_unchecked(scalar_index)
+            .get_unchecked(*other.get_unchecked(i) as usize);
+    }
+}
+
 pub fn fused_addassign_mul_scalar(octets: &mut [u8], other: &[u8], scalar: &Octet) {
     debug_assert_ne!(
         *scalar,
@@ -151,6 +243,11 @@ pub fn fused_addassign_mul_scalar(octets: &mut [u8], other: &[u8], scalar: &Octe
         if is_x86_feature_detected!("avx2") {
             unsafe {
                 return fused_addassign_mul_scalar_avx2(octets, other, scalar);
+            }
+        }
+        if is_x86_feature_detected!("ssse3") {
+            unsafe {
+                return fused_addassign_mul_scalar_ssse3(octets, other, scalar);
             }
         }
     }
@@ -221,12 +318,57 @@ unsafe fn add_assign_avx2(octets: &mut [u8], other: &[u8]) {
     }
 }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "ssse3")]
+unsafe fn add_assign_ssse3(octets: &mut [u8], other: &[u8]) {
+    #[cfg(target_arch = "x86")]
+    use std::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use std::arch::x86_64::*;
+
+    assert_eq!(octets.len(), other.len());
+    let self_ssse_ptr = octets.as_mut_ptr();
+    let other_ssse_ptr = other.as_ptr();
+    for i in 0..(octets.len() / 16) {
+        #[allow(clippy::cast_ptr_alignment)]
+        let self_vec = _mm_loadu_si128((self_ssse_ptr as *const __m128i).add(i));
+        #[allow(clippy::cast_ptr_alignment)]
+        let other_vec = _mm_loadu_si128((other_ssse_ptr as *const __m128i).add(i));
+        let result = _mm_xor_si128(self_vec, other_vec);
+        #[allow(clippy::cast_ptr_alignment)]
+        _mm_storeu_si128((self_ssse_ptr as *mut __m128i).add(i), result);
+    }
+
+    let remainder = octets.len() % 16;
+    let self_ptr = octets.as_mut_ptr();
+    let other_ptr = other.as_ptr();
+    for i in ((octets.len() - remainder) / 8)..(octets.len() / 8) {
+        #[allow(clippy::cast_ptr_alignment)]
+        let self_value = (self_ptr as *mut u64).add(i).read_unaligned();
+        #[allow(clippy::cast_ptr_alignment)]
+        let other_value = (other_ptr as *mut u64).add(i).read_unaligned();
+        let result = self_value ^ other_value;
+        #[allow(clippy::cast_ptr_alignment)]
+        (self_ptr as *mut u64).add(i).write_unaligned(result);
+    }
+
+    let remainder = octets.len() % 8;
+    for i in (octets.len() - remainder)..octets.len() {
+        *octets.get_unchecked_mut(i) ^= other.get_unchecked(i);
+    }
+}
+
 pub fn add_assign(octets: &mut [u8], other: &[u8]) {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
         if is_x86_feature_detected!("avx2") {
             unsafe {
                 return add_assign_avx2(octets, other);
+            }
+        }
+        if is_x86_feature_detected!("ssse3") {
+            unsafe {
+                return add_assign_ssse3(octets, other);
             }
         }
     }
