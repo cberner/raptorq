2dd4ec283f20ce0ae9853639973e2cd89c1e0c4a
Cem Karan <cem.f.karan.civ@mail.mil>
2020-01-09 10:10:40 -0500

    fix(matrix.rs): Added 'use serde::{Deserialize, Serialize};'
    I made a mistake when rebasing my commits on master and removed this line.
    Now fixed.

--- src/matrix.rs
+++ src/matrix.rs
@@ -3,6 +3,7 @@ use crate::octet::Octet;
 use crate::octets::fused_addassign_mul_scalar;
 use crate::octets::{add_assign, count_ones_and_nonzeros, mulassign_scalar};
 use crate::util::get_both_indices;
+use serde::{Deserialize, Serialize};
 
 pub trait OctetMatrix: Clone {
     fn new(
