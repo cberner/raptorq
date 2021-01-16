71d0c7ce2fba1d02f8a0b0ee07f2aab4571433b7
Christopher Berner <christopherberner@gmail.com>
2021-01-15 18:54:30 -0800

    Fix py_publish upload script

--- py_publish.sh
+++ py_publish.sh
@@ -2,9 +2,11 @@
 
 cd /raptorq
 yum install -y python3-pip
-pip3 install maturin
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=1.46.0
 source $HOME/.cargo/env
+
+pip3 install toml
+pip3 install maturin
 
 # xargs is just to merge the lines together into a single line
 maturin publish --cargo-extra-args="--features python" \
